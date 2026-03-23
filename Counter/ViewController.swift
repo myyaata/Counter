b051673b401f52a4b6beebc450ed7be09d072519
arinasilchenko <silchenkoarisha@gmail.com>
2026-03-23 22:45:39 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by арина сильченко on 23.03.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
