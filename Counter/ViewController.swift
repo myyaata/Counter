2d8b4f1828ab2534c949a0e43766e3f44a590daa
arinasilchenko <silchenkoarisha@gmail.com>
2026-03-24 00:19:07 +0300

    Реализована задача sprint_03

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -11,9 +11,54 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        CounterLabel.text = "0"
+    }
+    
+    private var count = 0
+    
+    private func printValue(_ count: Int)
+    {
+        CounterLabel.text = "Значение счётчика: \(count)"
     }
 
-
+    private func addToHistory(_ text: String) {
+        HistoryTextView.text = HistoryTextView.text + "\n\(Date.now.formatted(date: .numeric, time: .shortened)): " + text
+        let range = NSRange(location: HistoryTextView.text.count - 1, length: 1)
+        HistoryTextView.scrollRangeToVisible(range)
+    }
+    
+    @IBAction func IncreaseCounter(_ sender: UIButton) {
+        count += 1
+        printValue(count)
+        addToHistory("значение изменено на +1")
+    }
+    
+    
+    @IBAction func ReduceCounter(_ sender: UIButton) {
+        guard count != 0 else {
+            addToHistory("попытка уменьшить значение счётчика ниже 0")
+            return }
+        count -= 1
+        printValue(count)
+        addToHistory("значение изменено на -1")
+    }
+    
+    
+    @IBAction func ToZero(_ sender: UIButton) {
+        count = 0
+        printValue(count)
+        addToHistory("значение сброшено")
+    }
+    
+    @IBOutlet weak var HistoryTextView: UITextView!
+    
+    @IBOutlet weak var CounterLabel: UILabel!
+    
+    @IBOutlet weak var MinusButton: UIButton!
+    
+    @IBOutlet weak var PlusButton: UIButton!
+    
+    @IBOutlet weak var ToZeroButton: UIButton!
+    
 }
 
